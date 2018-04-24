d04ccc504e44785bd32255fc541adad11612cb3f
Edward Fang <edward.fang@berkeley.edu>
2018-04-23 22:49:23 -0700

    added dataloaders

--- preprocess_split.py
+++ preprocess_split.py
@@ -1,26 +1,24 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
-from scipy.misc import imresize
-
-def normalize(image):
-    # Normalize to pretrained criteria
-    shape = (224, 224, 3)
-    mean=[0.485, 0.456, 0.406]
-    std=[0.229, 0.224, 0.225]
-    image = imresize(image, shape)
-    image = image/256.0
-    for channel in range(3):
-        image[:,:,channel] = (image[:,:,channel] - mean[channel]) / std[channel]
-    return image
+from scipy.misc import imresize, imsave
     
 X = np.load('./data/X.npy')
 y = np.load('./data/Y.npy')
 
-X = np.array([normalize(image).transpose() for image in X])
-print(X.shape)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
-np.save('./data/train/X_train.npy', X_train)
-np.save('./data/train/y_train.npy', y_train)
-np.save('./data/test/X_test.npy', X_test)
-np.save('./data/test/y_test.npy', y_test)
+count = 0
+for x, y in zip(X_train, y_train):
+    if y == 0:
+        imsave('./data/train/not/' + str(count) + '.jpg', x)
+    else:
+        imsave('./data/train/cancer/' + str(count) + '.jpg', x)
+    count += 1
+
+count = 0
+for x, y in zip(X_test, y_test):
+    if y == 0:
+        imsave('./data/test/not/' + str(count) + '.jpg', x)
+    else:
+        imsave('./data/test/cancer/' + str(count) + '.jpg', x)
+    count += 1
